42cf0437d01bdc093f81cee24fc83ed5f3c6c290
yeseul <yeseul0900@handong.ac.kr>
2022-09-02 10:36:30 +0900

    first// make Student.java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> al=new ArrayList<Student>();
+
+    }
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Student {
+    int no;
+    String name;
+    int age;
+
+    public Student(int no, String name, int age){
+
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
